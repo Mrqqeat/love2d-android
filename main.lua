9b41b71a7d0821f3a384b5eefb60becff7ab3772
Claudio Pacchiega <claudio.pacchiega@gmail.com>
2018-03-30 11:03:34 +0200

    working under 0.10.2

--- main.lua
+++ main.lua
@@ -1,80 +1,127 @@
+-- modified a bit and translated to Italian from
 -- code by Iwan 'qubodup' Gabovitch
--- licesnse/terms: public domain (cc0)
+-- license/terms: public domain (cc0)
 -- meaning do what you want, no credit needed, you can delete this text too
 -- video tutorial: https://youtu.be/sArl6wypDfY
 -- you can make a bee.png from https://openclipart.org/detail/215877/cartoon-bee
 
+
 function love.load()
-  gw = love.graphics.getWidth() -- game width
-  gwp = gw/10 -- game widht "part"
+  -- Per fare debug da ZeroBrane
+  local debug = load_mod("mobdebug")
+  if debug then
+    debug.start()
+  end
 
-  gh=love.graphics.getHeight()
-  ghp = gh/10 -- game height "part"
+  gameWidth = love.graphics.getWidth()
+  gameHeight = love.graphics.getHeight()
 
-  b = { -- buttons
-    l = {gwp, ghp*2, gwp, ghp*6, 0}, -- left (left edge, top edge, width, height, timer)
-    r = {gwp*8, ghp*2, gwp, ghp*6, 0}, -- right
-    t = {gwp*2, ghp, gwp*6, ghp, 0}, -- top
-    b = {gwp*2, ghp*8, gwp*6, ghp, 0} -- bottom
+
+
+  -- sample player
+  player = { x = gameWidth / 2, y = gameHeight / 2, image = love.graphics.newImage('bee20small.png') }
+  -- sample player
+end
+
+-- l'area di gioco viene suddivisa in blocchi di coordinate 1/10
+-- rispetto all'area effettiva di gioco, questo semplifica il posizionamento
+-- dei singoli oggetti. L'idea viene ripresa da minetest (!)
+
+function px(x)
+  return gameWidth / 10 * x
+end
+function py(y)
+  return gameHeight / 10 * y
+end
+
+function buttons()
+  return {
+    left = { x = px(0), y = py(1), w = px(1), h = py(6) },
+    right = { x = px(7), y = py(1), w = px(1), h = py(6) },
+    top = { x = px(1), y = py(0), w = px(6), h = py(1) },
+    bottom = { x = px(1), y = py(7), w = px(6), h = py(1) }
   }
 
-  freq = 0.25 -- how many seconds until "button" can be pressed again, also visual aid
-
-  -- sample player
-  p = { gw/2, gh/2, love.graphics.newImage('bee20small.png') }
-  -- sample player
 end
 
 function love.draw()
   -- draw buttons
-  for i,v in pairs(b) do 
-    opacity = 100 + v[5] * 400 -- change opacity over time to indicate
-    love.graphics.setColor(255,255,255,opacity)
-    love.graphics.rectangle("fill",v[1],v[2],v[3],v[4])
+  for _, button in pairs(buttons()) do
+    love.graphics.setColor(255, 255, 255, 100)
+    love.graphics.rectangle("fill", button.x, button.y, button.w,
+        button.h)
   end
 
   -- sample player
-  love.graphics.setColor(255,255,255)
-  love.graphics.draw(p[3],p[1],p[2],0,1,1,40,40)
+  love.graphics.setColor(255, 255, 255)
+  love.graphics.draw(player.image, player.x, player.y, 0, 1, 1, 40, 40)
   -- sample player
 end
 
 function love.update(dt)
-  for i,v in pairs(b) do
-    if v[5] > 0 then
-      b[i][5] = v[5] - dt
-    else
-      b[i][5] = 0
+  if love.keyboard.isDown("up") then
+    up()
+  end
+  if love.keyboard.isDown("down") then
+    down()
+  end
+  if love.keyboard.isDown("left") then
+    left()
+  end
+  if love.keyboard.isDown("right") then
+    right()
+  end
+  local touches = love.touch.getTouches()
+
+  for i, id in pairs(touches) do
+    local x, y = love.touch.getPosition(id)
+    checkButtons(x, y)
+  end
+
+end
+
+function up()
+  player.y = player.y - 10
+end
+function down()
+  player.y = player.y + 10
+end
+function left()
+  player.x = player.x - 10
+end
+function right()
+  player.x = player.x + 10
+end
+
+function checkButtons(mx, my)
+  for name, button in pairs(buttons) do
+    -- verifica se abbiamo cliccato all'interno del rettangolo corrispondente
+    -- a questo bottone ma solo se t e' zero (evita di cliccare troppo velocemente)
+    if
+    -- se il click e' nell'intervallo di x in cui e' il bottone
+    (mx >= button.x) and mx <= (button.x + button.w)
+        -- se il click e' nell'intervallo di y in cui e' il bottone
+        and (my >= button.y) and my <= (button.y + button.h)
+    then
+      if name == "top" then
+        up()
+      elseif name == "left" then
+        left()
+      elseif name == "bottom" then
+        down()
+      elseif name == "right" then
+        right()
+      end
     end
   end
 end
 
-function love.keypressed(key)
-  if key == "up" then
-    p[2] = p[2] - 10
-  elseif key == "down" then
-    p[2] = p[2] + 10
-  elseif key == "right" then
-    p[1] = p[1] + 10
-  elseif key == "left" then
-    p[1] = p[1] - 10
+function load_mod(name)
+  local ok, mod = pcall(require, name)
+  if not ok then
+    mod = nil
   end
-end
-
-function love.mousepressed(mx,my)
-  for i,v in pairs(b) do
-    -- check collision and restrict allowed repeat click speed
-    if mx >= v[1] and mx <= v[1]+v[3] and my >= v[2] and my <= v[2] + v[4] and v[5] == 0 then
-      b[i][5] = freq
-      if i == "t" then
-        -- do what uparrow does
-      elseif i == "l" then
-        -- do what leftarrow does
-      elseif i == "b" then
-        -- do what downarrow does
-      elseif i == "r" then
-        -- do what rightarrow does
-      end
-    end	
+  if mod then
+    mod.start()
   end
 end
